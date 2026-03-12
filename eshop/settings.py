a4a8356dd4d708467afe7ce7697907e168963a73
Almas-Forhadi <almasforhasi1999@gmail.com>
2026-03-12 18:50:24 +0600

    postgreSQl added

--- eshop/settings.py
+++ eshop/settings.py
@@ -23,10 +23,13 @@ SECRET_KEY = env('SECRET_KEY')
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DEBUG = env.bool('DEBUG', default=False)
+DEBUG = env.bool('DEBUG')
 
 
 ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = ['https://muradmart.onrender.com','https://*.127.0.0.1']
+
 
 # Application definition
 
@@ -97,12 +100,19 @@ WSGI_APPLICATION = 'eshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=env('DATABASE_URL')
+    )
 }
+
 
 
 # Password validation
