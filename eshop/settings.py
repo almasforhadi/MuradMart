3dabc58995a010945d339ce0208e8aa065b37722
Almas-Forhadi <almasforhasi1999@gmail.com>
2026-03-12 21:27:17 +0600

    database error corrections

--- eshop/settings.py
+++ eshop/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 import environ
 import dj_database_url
 from pathlib import Path
@@ -109,7 +110,7 @@ WSGI_APPLICATION = 'eshop.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=env('DATABASE_URL')
+        default=os.environ.get("DATABASE_URL")
     )
 }
 
